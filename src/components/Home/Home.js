ec7f2655dbf96d97905b922828aaf20e66450654
Miroslav Tsanev <miroo2306@gmail.com>
2023-07-31 18:19:05 +0300

    added home sneaker images / header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import './home.css';
+import shoesBlue from '../../images/jordan-blue.png';
+import shoesRed from '../../images/jordan-red.png';
 
 export default function Home() {
     return (
-        <div>
-            
+        <div className="home">
+            <div className="home-container">
+                <div className="home-images-header">
+                    <div className="home-image-container">
+                        <img src={shoesRed} className="home-image" />
+                    </div>
+                    <div className="home-image-container">
+                        <img src={shoesBlue} className="home-image" />
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
